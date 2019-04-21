4ee3abea1efbc3194d883fba44175ee500f30f0d
unknown <jeandavid54@aol.com>
2019-04-21 15:47:26 +0200

    Commit message

---
+++ makehtmlfile.old.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import resources
+from resources.libs import *
+
+debug = False
+initial = False            # if True search using first letter
+Search_filter = ''         # title filter
+Search_name = 'action'     # source name
+
+#pass variable initial to libs
+resources.libs.initial=initial
+resources.libs.debug=debug
+resources.libs.Search_name=Search_name
+resources.libs.Search_filter=Search_filter
+
+def main():
+    match2=getmatch(Search_name)
+    print(type(match2))
+    buffer=makebuffer(match2)
+
+    f= open(".\\html\\"+Search_name+"_page.html","w+")
+    f.write(buffer)
+    f.close()
+    print("Processus terminé ! "+Search_name+"_page.html est créé dans le répertoire html !")
+
+if __name__ == '__main__':
+    main()
+
