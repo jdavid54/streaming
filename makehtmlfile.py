3ce854f5cbf1213b7c47e39deb5b366fc6ae2289
Jean David <jeandavid54@aol.com>
2021-11-20 14:51:04 +0100

    Add files via upload
    # update with new links only

--- makehtmlfile.py
+++ makehtmlfile.py
@@ -4,9 +4,14 @@ import resources
 from resources.libs import getmatch, makebuffer
 import time
 
+# last total = 622
+# doublon = 3
+
 def main():
     debug = False
     initial = False        # if True search using first letter
+    do_all = False         # if True make html page from scratch else complete with old one data
+    
     Search_filter = ''     # title filter
     Search_name = 'nr'     # change here nr, nr2, nr3, n4, n5 .... ! for initial search, use argument -i
     text_file = 'data/match.txt'
@@ -21,8 +26,6 @@ def main():
         initial = True
     if '-n' in args:
         Search_name = args[args.index('-n')+1]
-    #else:
-    #    Search_name = 'nr'
     
     #pass variable initial to libs
     resources.libs.initial = initial
@@ -30,21 +33,25 @@ def main():
     resources.libs.Search_name = Search_name
     resources.libs.Search_filter = Search_filter
     resources.libs.data_file = text_file
+    resources.libs.do_all = do_all
     
     match2 = getmatch(Search_name)  # get the file from 'nr', or,'nr2', ...
-    #print(type(match2))
+
     buffer = makebuffer(match2)
     end = time.time()
     print('Time of process :',end - start,'s')
 
-    f= open("html/"+Search_name+"_vignette.html","w+")
-    f.write(buffer)
-    f.close()
-    print("Processus terminé ! "+Search_name+"_vignette.html est créé dans le répertoire html !")
+    with open("html/"+Search_name+"_vignette.html","w+") as f:
+        f.write(buffer)
+        #f.close()
+        print("Processus terminé ! " + Search_name + "_vignette.html est créé dans le répertoire html !")
+    
+    # save to local web server
+    if Search_name == 'nr':
+        with open("/var/www/html/streaming/html/nr_vignette.html","w+") as f:
+            f.write(buffer)
 
-'''
 if __name__ == '__main__':
     main()
-'''
-main()
+
 
